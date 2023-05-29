fc0bba92a7e9116ec3d25eb6d71b1b779137583d
joy <leejoys.lab@gmail.com>
2023-05-29 13:58:09 +0900

    0529_대소문자 바꿔서 출력하기

---
+++ 0529_대소문자 바꿔서 출력하기.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+  result = [];
+
+  for (const s of str) {
+    const lowerS = s.toLowerCase();
+    const upperS = s.toUpperCase();
+
+    if (s === lowerS) {
+      result.push(upperS);
+    } else {
+      result.push(lowerS);
+    }
+  }
+
+  console.log(result.join(""));
+});
