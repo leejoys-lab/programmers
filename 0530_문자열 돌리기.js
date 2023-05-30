a0e6986ab22297d9751ab5bea3e189b03c3a6841
joy <leejoys.lab@gmail.com>
2023-05-30 21:02:52 +0900

    0530_문자열 돌리기

---
+++ 0530_문자열 돌리기.js
@@ -0,0 +1,36 @@
+// 나의 풀이
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  const str = input[0];
+  for (const result of str) {
+    console.log(result);
+  }
+});
+
+/* 
+// 참고 풀이
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    [...str].forEach(c => console.log(c))
+}); 
+*/
