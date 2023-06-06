e1ba6f6a8e1152541a9fc895990ee9c3fb92344f
joy <leejoys.lab@gmail.com>
2023-06-06 17:36:37 +0900

    0606_배열 만들기_푸는중

---
+++ 0606_배열만들기.js
@@ -0,0 +1,16 @@
+/* 
+정수 l과 r이 주어졌을 때, 
+l <= 정수(number) <= r 중에서 숫자 "0"과 "5"로만 이루어진 모든 정수를 
+오름차순으로 저장한 배열을 return 해 주세요.
+*** 만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
+ */
+const l = 5;
+const r = 555;
+solution(l, r);
+
+function solution(l, r) {}
+
+/* 
+arr[i]의 값을 문자로 바꾼다
+arr[i][0]부터 arr[i][length]까지의 숫자가 
+*/
