7ceec5dfcc16fd0de9e564e199a13d76487a2d33
joy <leejoys.lab@gmail.com>
2023-06-07 13:34:32 +0900

    0607_카운트업

---
+++ 0607_카운트업.js
@@ -0,0 +1,11 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+function solution(start, end) {
+  var answer = [];
+
+  for (let i = start; i <= end; i++) {
+    answer.push(i);
+  }
+
+  return answer;
+}
