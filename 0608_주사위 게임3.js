876828251ba9950a79e5ed27100dbf7766a6b732
joy <leejoys.lab@gmail.com>
2023-06-09 16:08:29 +0900

    0609_주사위 게임3

--- 0608_주사위 게임3.js
+++ 0608_주사위 게임3.js
@@ -1,33 +1,55 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181916
+// solution(2, 2, 2, 2);
+// solution(4, 1, 4, 4);
+// solution(6, 3, 3, 6);
+// solution(2, 5, 2, 6);
+// solution(6, 4, 2, 5);
 
-solution(2, 2, 2, 1);
-// solution(4,1,4,4)
 
+// 메소드 다 필요없어...
 function solution(a, b, c, d) {
-  console.log(Math.min());
-  // return answer;
+  if (a === b && a === c && a === d) return 1111 * a
+
+  if (a === b && a === c) return (10 * a + d) ** 2
+  if (a === b && a === d) return (10 * a + c) ** 2
+  if (a === c && a === d) return (10 * a + b) ** 2
+  if (b === c && b === d) return (10 * b + a) ** 2
+
+  if (a === b && c === d) return (a + c) * Math.abs(a - c)
+  if (a === c && b === d) return (a + b) * Math.abs(a - b)
+  if (a === d && b === c) return (a + b) * Math.abs(a - b)
+
+  if (a === b) return c * d
+  if (a === c) return b * d
+  if (a === d) return b * c
+  if (b === c) return a * d
+  if (b === d) return a * c
+  if (c === d) return a * b
+
+  return Math.min(a, b, c, d)
 }
+
 
 /* 
 주사위는 총 4개
-
 1. 네 주사위에서 나온 숫자가 모두 p로 같다면
 => 1111 × p점을 얻습니다.
-a === b === c === d === p => 1111*p
+(a===b && b===c && c===d)
+=> 1111*p
 
 2. 세 주사위에서 나온 숫자가 p로 같고
 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면
 => (10 × p + q)2 점을 얻습니다.
-abc === p / d !== p === q
-acd === p / b !== p === q
-abd === p / c !== p === q
-bcd === p / a !== p === q
+abc === p , d === q !== p 
+acd === p , b === q !== p 
+abd === p , c === q !== p
+bcd === p , a === q !== p
 => (10 * p + q) * 2
+
 
 3. 주사위가 두 개씩 같은 값이 나오고,
 나온 숫자를 각각 p, q(p ≠ q)라고 한다면
 (p + q) × |p - q|점을 얻습니다.
-abcd
 ab === p / cd === q / p !== q
 ac === p / bd === q / p !== q
 ad === p / bc === q / p !== q
@@ -47,7 +69,6 @@ bc === p / a === q / d === r / p !== q !== r
 나온 숫자 중 가장 작은 숫자 만큼의 점수를 얻습니다.
 a !== b !== c !== d
 =>Math.min(a,b,c,d)
-
 -------------------------------------------
 => 네개 주사위를 굴렸을 때의 점수를 return
 */
