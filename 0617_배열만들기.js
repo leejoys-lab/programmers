17088eff51c864e11264bf9433cd71277a234e18
joy <leejoys.lab@gmail.com>
2023-06-17 23:58:48 +0900

    0617_배열만들기

---
+++ 0617_배열만들기.js
@@ -0,0 +1,12 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181901
+
+function solution(n, k) {
+    const answer = [];
+    
+    for(let i=1; i<=n; i++){
+        if(i%k === 0){
+            answer.push(i)   
+        }
+    }
+    return answer;
+}
