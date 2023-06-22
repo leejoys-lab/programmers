ef7fbdbfc2981d9a21b3a78a5614ea090f02001a
joy <leejoys.lab@gmail.com>
2023-06-22 14:16:44 +0900

    0622_주사위게임1

---
+++ 0622_주사위 게임1.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181839
+
+function solution(a, b) {
+  let answer = 0;
+
+  if (a % 2 === 1 && b % 2 === 1) {
+    answer = a * a + b * b;
+  } else if (a % 2 === 1 || b % 2 === 1) {
+    answer = a + b + (a + b);
+  } else {
+    answer = Math.abs(a - b);
+  }
+
+  return answer;
+}
