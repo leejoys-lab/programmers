73835e9247f19dffdd6c10e0d866c2b00a3e067e
joy <leejoys.lab@gmail.com>
2023-07-25 23:55:07 +0900

    날짜 비교하기

---
+++ 0725_날짜 비교하기.js
@@ -0,0 +1,15 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181838
+
+function solution(date1, date2) {
+    if(date1 == date2) return 0
+    
+     if (date1[0] < date2[0]) {
+        return 1;
+    } else if (date1[0] === date2[0] && date1[1] < date2[1]) {
+        return 1;
+    } else if (date1[0] === date2[0] && date1[1] === date2[1] && date1[2] < date2[2]) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
